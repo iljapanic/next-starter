db0df8d676f89aabaccafa8198b7a8bc20ad59c8
Ilja Panic <ilja@iljapanic.com>
2023-03-24 13:43:04 +0100

    update tailwind.config.js and jsconfig.js, udpate npm packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,40 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* base colors scales */
+        gray: colors.stone,
+
+        /* social colors */
+        rss: '#f26522',
+        linkedin: '#0072b1',
+        twitter: '#1DA1F2',
+        github: '#171515',
+        instagram: '#cd486b',
+        facebook: '#3b5998',
+      },
+      fontSize: {
+        '2xs': '.575rem',
+      },
+      fontFamily: {
+        sans: [...fontFamily.sans],
+        serif: [...fontFamily.serif],
+        system: [...fontFamily.sans],
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        lg: '0',
+      },
+    },
   },
-  plugins: [],
 }
